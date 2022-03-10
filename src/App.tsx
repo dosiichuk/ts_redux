f6021f799f6103a90317cd9dc6edcc8b2f708350
Dmytro <dmytro.osiychuk@gmail.com>
2022-03-10 20:17:50 +0100

    Complete redux layout in TS - exemplary implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './state';
+import RepositoriesList from './components/RepositoriesList';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h2 className="App-header">Search for a Package</h2>
+        <RepositoriesList />
+      </div>
+    </Provider>
   );
 }
 
